fd21b8fbaf9dda6bcb2e2c72cc62b8af3eb1a729
Melissa <ma.pena94@gmail.com>
2022-09-17 10:52:47 -0700

    wave 1completed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,11 +1,43 @@
 # ------------- WAVE 1 --------------------
 #Melissa test
+
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        new_movie = {}
+        new_movie["title"]= title
+        new_movie["genre"]= genre
+        new_movie["rating"]= rating
+        
+        return new_movie
+    
+    return None
+
+def add_to_watched(user_data, movie):
+    
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    
+    for movie in user_data["watchlist"]:
+        if movie["title"]== title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+
+    return user_data
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+
+
 
 
 # -----------------------------------------
